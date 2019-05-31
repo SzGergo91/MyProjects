2519d8dd713d721343fb98a32f89a0279394cd1b
Gergo Szabo <BOZONTH91@yahoo.com>
2019-05-31 20:12:26 +0100

    New Calculator
    New Calculator/95%

--- Projektek/MainProjects/Calculator.js
+++ Projektek/MainProjects/Calculator.js
@@ -1,8 +1,14 @@
 let $globalStepper="";
 let $globalInputValue="";
 let $globalResult=0;
-let $operatorActive=false;
-let $operatorvalue="";
+let $globalOperators=false;
+
+let $globalContainer=[];
+let $globalpart1="";
+let $globalpart2="";
+let $globaleredmeny=0;
+let $globalB="";
+let $dotcounter=0;
 
 $(document).ready(()=>{
 	$('.buttons').on('mousedown',event=>{
@@ -58,140 +64,310 @@ $(document).ready(()=>{
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 	$('#1').on('click',()=>{
 		$globalInputValue="1";
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 	$('#2').on('click',()=>{
 		$globalInputValue="2";
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 	$('#3').on('click',()=>{
 		$globalInputValue="3";
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 	$('#4').on('click',()=>{
 		$globalInputValue="4";
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 	$('#5').on('click',()=>{
 		$globalInputValue="5";
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 	$('#6').on('click',()=>{
 		$globalInputValue="6";
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 	$('#7').on('click',()=>{
 		$globalInputValue="7";
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 	$('#8').on('click',()=>{
 		$globalInputValue="8";
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 	$('#9').on('click',()=>{
 		$globalInputValue="9";
 		$globalStepper+=$globalInputValue;
 		$('#Input').val($globalStepper);
 		$globalInputValue="";
+		$globalOperators=false;
 	})
 
 	//Setting the Operators
-	$('#Ac').on('click',()=>{
+	$('#plus').on('click',()=>{
+		if(!$globalOperators)
+		{
+		$globalInputValue="+";
+		$globalStepper+=$globalInputValue;
+		$('#Input').val($globalStepper);
+		$globalInputValue="";
+		$globalOperators=true;
+		}
+	})
+    $('#minus').on('click',()=>{
+    	if(!$globalOperators)
+    	{
+		$globalInputValue="-";
+		$globalStepper+=$globalInputValue;
+		$('#Input').val($globalStepper);
+		$globalInputValue="";
+		$globalOperators=true;
+		}
+	})
+	$('#times').on('click',()=>{
+		if(!$globalOperators)
+		{
+		$globalInputValue="*";
+		$globalStepper+=$globalInputValue;
+		$('#Input').val($globalStepper);
+		$globalInputValue="";
+		$globalOperators=true;
+		}
+	})
+	$('#division').on('click',()=>{
+		if(!$globalOperators)
+		{
+		$globalInputValue="/";
+		$globalStepper+=$globalInputValue;
+		$('#Input').val($globalStepper);
+		$globalInputValue="";
+		$globalOperators=true;
+		}
+	})
+	$('#dot').on('click',()=>{
+		if(!$globalOperators)
+		{
+		$globalInputValue=".";
+		$globalStepper+=$globalInputValue;
+		$('#Input').val($globalStepper);
+		$globalInputValue="";
+		$globalOperators=true;
+		}
+	})
+
+	$('#Ac').on('click',()=>
+	{
 		$globalStepper="";
 		$globalInputValue="";
 		$globalResult=0;
-		$operatorActive=false;
 		$('#Input').val(null);
 		$('#Result').html("0");
+		$globalContainer=[];
+		$globalpart1="";
+		$globalpart2="";
+		$globalB="";
+		$globaleredmeny=0;
 	})
 
-	//I.Bg form
-	//$GlobalInputValue for the 1 characters!
-	//$GlobalStepper entire line!
-	//$GlobalResult input value
+	//Setting the Main
 
-	//II.Frontend form
-	//$('#Input').val the physical see!
-	//$('#Result') the see result!
-
+	//Ezt maskepp kell csialni! A karakterekbol allo tombot atkell alakitani u hogy szamok es operatorokbol alljanak!
 	$('#Equal').on('click',()=>{
-		if(!$operatorActive){
-			$globalResult=Number($('#Input').val());
-			$('#Result').html(`${$globalResult}`);
-			$operatorActive=false;
+		if($globalStepper.length === 0){
+			$('#Result').html("0");
 		}
-		else {
-			switch($operatorvalue){
-				case "+" :
-					$globalResult+=Number($('#Input').val());
-					$('#Result').html(`${$globalResult}`);
-					$operatorActive=false;
-					break;
-				case "-":
-					$globalResult-=Number($('#Input').val());
-					$('#Result').html(`${$globalResult}`);
-					$operatorActive=false;
-					break;
+		else
+		{
+			for(let i=0;i<$globalStepper.length;i++)
+			{
+				let c=Number($globalStepper.charAt(i));
+				if (isNaN(c))
+				{
+					$globalContainer.push($globalStepper.charAt(i));
+				}
+				else
+				{
+					$globalContainer.push(c);
+				}
 			}
-					Number($('#Input').val(`${globalResult}`));
-					$globalStepper=0;
+					//Verifikalni hany . szerepel benne mert nem lehet osszeadni 2 tizedes szamot! 
+					//Ahol van pont atalakitsuk szamma es ugy hasznaljuk!
+					$globalStepper="";
+					$globalInputValue="";
+					$('#Input').val(null);
+
+					for(let i=0;i<$globalContainer.length;i++)
+					{
+						if($globalContainer[i] === "."){
+							$dotcounter+=1;
+						}
+					}
+
+					if($dotcounter>1)
+					{
+						for(let i=0;i<$globalContainer.length;i++)
+						{
+							if($globalContainer[i] === ".")
+							{
+								$globalpart2+=".";
+								for(j=i;j<$globalContainer.length;j++){
+									if(typeof($globalContainer[j]) === "number")
+									{
+										$globalpart2+=$globalContainer[j];
+									}
+
+									
+								}
+							}
+							//Nem pont akkor hozzaadom
+							$globalpart2+=$globalContainer[i];
+						}
+					}
+
+					for(let i=0;i<$globalContainer.length;i++)
+					{
+						//Mergeles
+							//console.log($globalContainer[i]);
+						if(typeof($globalContainer[i]) === "number")
+						{
+							//Megkell nezni hogy vege van-e az ertekeknek!if(!$globalContainer[i].length)
+							if(i!==$globalContainer.length-1)
+							{	
+								$globalpart1+=$globalContainer[i];
+							}
+							else
+							{
+								$globalpart1+=$globalContainer[i];
+								switch($globalB)
+								{
+									case "" :
+										$globaleredmeny=Number($globalpart1);
+										break;
+									case "+": 
+							  			$globaleredmeny=Number($globalpart2)+Number($globalpart1);
+							  			break;
+							  		case "-": 
+							  			$globaleredmeny=Number($globalpart2)-Number($globalpart1);
+							  			break;
+							  		case "*": 
+							  			$globaleredmeny=Number($globalpart2)*Number($globalpart1);
+							  			break;
+							  		case "/": 
+							  			$globaleredmeny=Number($globalpart2)/Number($globalpart1);
+							  			break;
+							  		case ".": 
+							  			$globaleredmeny=Number($globalpart2+$globalB+$globalpart1);
+							  			break;
+
+								}
+							}
+						}
+
+				
+						else
+						{
+							if($globalB ==="")
+							{
+								//If its first operator and last operator!
+								//A Containeres if mukodik!
+								if(i === $globalContainer.length-1)
+								{	
+									$globaleredmeny=Number($globalpart1);	
+								}
+								//Ebbe az else agba nem lep bele!
+								else
+								{
+									$globalB=$globalContainer[i];
+									$globalpart2=$globalpart1;
+									$globalpart1="";
+								}
+								
+							}
+							else
+							  {
+							  	//if its not the first operator but last!
+							  	//Megcsinalni tombbel!
+							  	if(i!==$globalContainer.length-1)
+							  	{
+							  	switch($globalB)
+							  	{
+							  		case "+": 
+							  			$globaleredmeny=Number($globalpart2)+Number($globalpart1);
+							  		break;
+							  		case "-": 
+							  			$globaleredmeny=Number($globalpart2)-Number($globalpart1);
+							  		break;
+							  		case "*": 
+							  			$globaleredmeny=Number($globalpart2)*Number($globalpart1);
+							  		break;
+							  		case "/": 
+							  			$globaleredmeny=Number($globalpart2)/Number($globalpart1);
+							  		break;
+							  		case ".": 
+							  			$globaleredmeny=Number(String($globalpart2+$globalB+$globalpart1));
+							  		break;
+							  	}
+
+							  		$globalpart2=String($globaleredmeny);
+							  		$globalpart1="";
+							  		$globalB=$globalContainer[i];
+							  	}
+
+
+							}
+						}
+						
+					}
+
+
+					$globalContainer=[];
+					$('#Result').html(`${$globaleredmeny}`);
+					$('#Input').val(`${$globaleredmeny}`);
+					$globalStepper=String($globaleredmeny);
+					console.log(String($globalpart2+$globalB+$globalpart1));
+					$globalpart1="";
+					$globalpart2="";
+					$globalB="";
+					$globalInputValue="";
+					
 		}
 	})
 
 
-	$('#plus').on('click',()=>{
-		if($operatorActive){
-			$globalResult+=Number($('#Input').val());
-			$('#Result').html(`${$globalResult}`);
-			$globalStepper="";
-			Number($('#Input').val(null));
-		}
-		else
-		{
-		$globalResult=Number($('#Input').val());
-		$('#Result').html(`${$globalResult}`);
-		$globalStepper="";
-		Number($('#Input').val(null));
-		$operatorActive=true;
-		$operatorvalue="+";
-		}
-	})
 
-	$('#minus').on('click',()=>{
-		if($operatorActive){
-			$globalResult-=Number($('#Input').val());
-			$('#Result').html(`${$globalResult}`);
-			$globalStepper="";
-			Number($('#Input').val(null));
-		}
-		else
-		{
-		$globalResult=Number($('#Input').val());
-		$('#Result').html(`${$globalResult}`);
-		$globalStepper="";
-		Number($('#Input').val(null));
-		$operatorActive=true;
-		$operatorvalue="-";
-		}
-	})
+
+
+
+
+
+
+
 
 })
